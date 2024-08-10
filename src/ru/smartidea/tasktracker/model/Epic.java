488e274dd9a2170635e66ce970805a65448dfb6d
Maxim Skoblenkov <goofy.net@yandex.ru>
2024-08-10 15:41:50 +0400

    Sprint_8: Добавил класс Epic строку 29

--- src/ru/smartidea/tasktracker/model/Epic.java
+++ src/ru/smartidea/tasktracker/model/Epic.java
@@ -26,8 +26,7 @@ public class Epic extends Task {
         this.subtaskIds = subtaskIds;
     }
 
-    public Epic(int id, String name, TaskStatus status, String description, LocalDateTime startTime,
-                Duration duration) {
+    public Epic(int id, String name, TaskStatus status, String description, LocalDateTime startTime, Duration duration) {
         super(id, name, description, status, startTime, duration);
     }
 
