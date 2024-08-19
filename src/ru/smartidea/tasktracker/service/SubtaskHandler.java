11428e3c43ec570d707a922664e79f68ca268c5d
SkoblenkovMaxim <136118554+SkoblenkovMaxim@users.noreply.github.com>
2024-08-19 18:31:20 +0400

    Update SubtaskHandler.java

--- src/ru/smartidea/tasktracker/service/SubtaskHandler.java
+++ src/ru/smartidea/tasktracker/service/SubtaskHandler.java
@@ -51,7 +51,6 @@ public class SubtaskHandler extends BaseHttpHandler implements HttpHandler {
                 } else {
                     throw new RuntimeException("Данные не переданы");
                 }
-                break;
             case "GET":
                 Integer id = getIdFromPath(httpExchange.getRequestURI().getPath());
                 if (id == null) {
@@ -75,7 +74,6 @@ public class SubtaskHandler extends BaseHttpHandler implements HttpHandler {
                         sendServerError(httpExchange);
                     }
                 }
-                break;
             case "DELETE":
                 Integer deleteId = getIdFromPath(httpExchange.getRequestURI().getPath());
                 try {
@@ -90,14 +88,12 @@ public class SubtaskHandler extends BaseHttpHandler implements HttpHandler {
                 } catch (Exception e) {
                     sendServerError(httpExchange);
                 }
-                break;
             default:
                 try {
                     sendNotFound(httpExchange, "Такого запроса не существует");
                 } catch (Exception e) {
                     sendServerError(httpExchange);
                 }
-                break;
         }
     }
 }
