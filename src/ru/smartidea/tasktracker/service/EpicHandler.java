a4a0dd311d95004f469210196ed13f80aafd23ed
SkoblenkovMaxim <136118554+SkoblenkovMaxim@users.noreply.github.com>
2024-08-19 18:16:46 +0400

    Update EpicHandler.java

--- src/ru/smartidea/tasktracker/service/EpicHandler.java
+++ src/ru/smartidea/tasktracker/service/EpicHandler.java
@@ -4,7 +4,6 @@ import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import ru.smartidea.tasktracker.model.Epic;
-import ru.smartidea.tasktracker.model.Task;
 
 import java.io.IOException;
 import java.io.InputStream;
